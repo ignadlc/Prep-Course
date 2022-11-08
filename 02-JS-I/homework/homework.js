30be4860d865a559e558262996142008675acd41
ignadlc <ignaciodelacolina10@gmail.com>
2022-11-08 11:13:17 -0300

    finish

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -195,13 +195,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-if(numero>0){
-  return "Es positivo"
-}else if(numero<0){
-  return "Es negativo"
-}else{
-return false
-}
+  if(numero>0){
+    return "Es positivo"
+  }else if(numero<0){
+    return "Es negativo"
+  }else{
+    return false
+  }
 }
 
 function agregarSimboloExclamacion(str) {
